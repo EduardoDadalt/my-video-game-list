cee346391b99c618f47531f23ca476ba755a66f4
Eduardo Dadalt <eduardodadalt1@gmail.com>
2024-01-21 21:13:31 -0300

    Fix authentication

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -3,12 +3,48 @@ import { env } from "@/env";
 import { db } from "@/server/db";
 import { verifyPassword } from "@/util/cripto";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import { getServerSession, type NextAuthOptions } from "next-auth";
+import {
+  DefaultSession,
+  getServerSession,
+  type NextAuthOptions,
+} from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import DiscordProvider from "next-auth/providers/discord";
 import { z } from "zod";
 
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+      // ...other properties
+      // role: UserRole;
+    } & DefaultSession["user"];
+  }
+
+  // interface User {
+  //   // ...other properties
+  //   // role: UserRole;
+  // }
+}
 export const authOptions: NextAuthOptions = {
+  callbacks: {
+    jwt: async ({ account, token, user }) => {
+      if (!!user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    session: ({ session, user, token }) => {
+      const id = token.id;
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id,
+        },
+      };
+    },
+  },
   session: { strategy: "jwt" },
   pages: {
     signIn: "/auth/login",

6cccb89a31814d52ff76ceda9f938c19d9606002
Eduardo Dadalt <eduardodadalt1@gmail.com>
2024-03-06 23:09:57 -0300

    Fix tabOfPages

--- src/app/[locale]/game/[gameId]/_components/tabsOfPages.tsx
+++ src/app/[locale]/game/[gameId]/_components/tabsOfPages.tsx
@@ -1,42 +1,47 @@
 "use client";
-import { usePathname } from "next/navigation";
-import { type ReactNode } from "react";
-import { FiHome } from "react-icons/fi";
-import { GrGallery } from "react-icons/gr";
-import { FiUsers } from "react-icons/fi";
 import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { FiHome, FiUsers } from "react-icons/fi";
+import { GrGallery } from "react-icons/gr";
 
-function TabTitle({ icon, text }: { icon: ReactNode; text: string }) {
-  return (
-    <div className="flex items-center gap-2">
-      {icon} {text}
-    </div>
-  );
-}
+const tabs: {
+  href: string;
+  icon: React.ReactNode;
+  label: string;
+}[] = [
+  {
+    href: "",
+    icon: <FiHome />,
+    label: "Home",
+  },
+  {
+    href: "/characters",
+    icon: <FiUsers />,
+    label: "Characters",
+  },
+  {
+    href: "/images",
+    icon: <GrGallery />,
+    label: "Images",
+  },
+];
+
 export function TabOfPages({ gameId }: { gameId: string }) {
   const pathname = usePathname();
   const split = pathname.split(gameId);
-  const homeUrl = split[0] + "/" + gameId;
+  const homeUrl = split[0] + gameId;
 
   return (
-    <Tabs aria-label="Options" color="primary">
+    <Tabs aria-label="Options" color="primary" value={pathname}>
       <TabsList className="*:*:flex *:*:flex-row *:*:items-center *:*:gap-2">
-        <TabsTrigger value="home" asChild>
-          <Link href={homeUrl}>
-            <FiHome /> Home
-          </Link>
-        </TabsTrigger>
-        <TabsTrigger value="characters">
-          <Link href={homeUrl + "/characters"}>
-            <FiUsers /> Characters
-          </Link>
-        </TabsTrigger>
-        <TabsTrigger value="images">
-          <Link href={homeUrl + "/images"}>
-            <GrGallery /> Images
-          </Link>
-        </TabsTrigger>
+        {tabs.map(({ icon, href, label }) => (
+          <TabsTrigger key={href} value={homeUrl + href} asChild>
+            <Link href={homeUrl + href}>
+              {icon} {label}
+            </Link>
+          </TabsTrigger>
+        ))}
       </TabsList>
     </Tabs>
   );
